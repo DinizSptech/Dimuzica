f58a5ae589758dcda2fef680f3a82ed4f084dd01
DinizSptech <mateus.leite@sptech.school>
2024-11-12 13:55:45 -0300

    Login e cadastro com o BD feitos

---
+++ public/main.js
@@ -0,0 +1,14 @@
+  function local_login(){
+    window.location.href = 'login.html'
+  }
+  function local_cadastro(){
+    window.location.href = 'cadastro.html'
+  }
+
+  function login(){
+
+  }
+
+  function cadastro(){
+
+  }
