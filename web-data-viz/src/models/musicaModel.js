bbb57853550b5a8ac8ac481bed1ee8d437339aa0
DinizSptech <mateus.leite@sptech.school>
2024-11-26 19:13:46 -0300

    Correções

--- web-data-viz/src/models/musicaModel.js
+++ web-data-viz/src/models/musicaModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function insert_curtida(musica, usuario) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function insert_curtida(): ")
     var instrucaoSql = `
-        INSERT INTO curtida VALUES (${usuario}, ${musica});
+        INSERT INTO curtida VALUES (${usuario}, ${musica}, now());
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
